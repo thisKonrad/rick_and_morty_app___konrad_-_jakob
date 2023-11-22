a46df0048cfb39e70defd2ea3bc20408f5ddc3b2
this.Konrad <konradlebt@gmail.com>
2023-11-22 14:50:19 +0100

    enhanced the basic createCharatcterCard innerHTML functionality

--- components/card/card.js
+++ components/card/card.js
@@ -1,8 +1,10 @@
 export function createCharacterCard() {
-//the status, type and occurrences values
-    const cardSourceUrl = null;
-    const characterName = null;
+
+    const cardSourceUrl = `${cardSrc}`;
+    const characterName = `${charName}`;
     const statusInfo = `${state}`;
+    const typeInfo = `${type}`;
+    const occurnessInfo = `${occurness}`
 
     cardContainer.innerHTML = `<li class="card">
       <div class="card__image-container">
@@ -14,17 +16,16 @@ export function createCharacterCard() {
         <div class="card__image-gradient"></div>
       </div>
       <div class="card__content">
-        <h2 class="card__title">Rick Sanchez</h2>
+        <h2 class="card__title">${characterName}</h2>
         <dl class="card__info">
           <dt class="card__info-title">Status</dt>
           <dd class="card__info-description">${statusInfo}</dd>
           <dt class="card__info-title">Type</dt>
-          <dd class="card__info-description"></dd>
+          <dd class="card__info-description">${typeInfo}</dd>
           <dt class="card__info-title">Occurrences</dt>
-          <dd class="card__info-description">51</dd>
+          <dd class="card__info-description">${occurnessInfo}</dd>
         </dl>
       </div>
-    </li>
-  </ul>`;
+    </li>`;
 }
 
