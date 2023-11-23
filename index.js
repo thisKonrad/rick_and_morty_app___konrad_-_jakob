207cef54bc3f04065acfcb7b1f66983e85d8c566
Jakob Gross <jakobgross@gmail.com>
2023-11-23 11:00:58 +0100

    buttons work

--- index.js
+++ index.js
@@ -11,41 +11,77 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 let page = 1;
+let maxPage;
 
-//fetch API
 let charactersFetched;
-let fetchUrl = `https://rickandmortyapi.com/api/character/?page=${page}`;
+
 async function fetchCharacters(index) {
+  console.log("hi");
+
   try {
+    let fetchUrl = `https://rickandmortyapi.com/api/character/?page=${page}`;
     const response = await fetch(fetchUrl);
     const data = await response.json();
     charactersFetched = data;
+    maxPage = charactersFetched.info.pages;
+    const resultArray = charactersFetched.results;
+
+    cardContainer.innerHTML = "";
+
+    resultArray.forEach((elem, i) => {
+      // console.log("inside twentyCard", elem, i);
+      cardContainer.append(createCharacterCard(i, charactersFetched));
+    });
   } catch (error) {
     console.log(error);
   }
   return;
 }
+pagination.innerText = `${page}/${42}`;
+fetchCharacters(page);
 
-await fetchCharacters(page);
+nextButton.addEventListener("click", () => {
+  if (page === maxPage) {
+    page = 1;
+    fetchCharacters(page);
+    pagination.innerText = `${page}/${maxPage}`;
+  } else {
+    page++;
+    fetchCharacters(page);
+    pagination.innerText = `${page}/${maxPage}`;
+  }
+});
 
+prevButton.addEventListener("click", () => {
+  if (page === 1) {
+    page = maxPage;
+    fetchCharacters(page);
+    pagination.innerText = `${page}/${maxPage}`;
+  } else {
+    page--;
+    fetchCharacters(page);
+    pagination.innerText = `${page}/${maxPage}`;
+  }
+});
 // States
-const maxPage = charactersFetched.info.pages; //42
+// const maxPage = charactersFetched.info.pages; //42
 
 const searchQuery = "";
-// let index = 1;
 
-const resultArray = charactersFetched.results;
-console.log("resultArray: ", resultArray);
+// const resultArray = charactersFetched.results;
+// console.log("resultArray: ", resultArray);
 
-function twentyCards() {
-  cardContainer.innerHTML = "";
-  resultArray.forEach((elem, i) => {
-    console.log("inside twentyCrad", elem, i);
-    createCharacterCard(i, charactersFetched);
-  });
-}
+// function twentyCards() {
+// cardContainer.innerHTML = "";
 
-twentyCards();
+// // await fetchCharacters(page)
+// resultArray.forEach((elem, i) => {
+//   console.log("inside twentyCard", elem, i);
+//   cardContainer.append(createCharacterCard(i, charactersFetched));
+//   });
+// }
+
+// twentyCards();
 // Use your knowledge about fetching to get the first 20 characters from the API. You can find the correct API endpoint in the docs.
 // Import the createCharacterCard function.
 // After successfully fetching the character data, use array methods to create an HTML card for each character and append it to the cardContainer.
