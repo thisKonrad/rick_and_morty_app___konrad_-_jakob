5b2a7f1fee7acbb316e2c7eb60c8f850076ca48e
this.Konrad <konradlebt@gmail.com>
2023-11-23 16:41:14 +0100

    add some css

--- components/nav-pagination/nav-pagination.js
+++ components/nav-pagination/nav-pagination.js
@@ -1,15 +1,16 @@
 export function createPagination(){
 
-    const main = document.querySelector('main');
+    let main = document.querySelector('main');
     const navBar = document.createElement('nav');
-    navBar.classList.add('class="navigation"');
+    navBar.classList.add('class="navigation');
     navBar.setAttribute('data-js','navigation')
 
     const pagination = document.createElement('span');
     pagination.classList.add('navigation__pagination');
     pagination.setAttribute('data-js','pagination');
-    pagination.innerText =`${page}/${42}`;
+    //pagination.innerText =``;
 
+    main.prepend('afterend',navBar);
     navBar.append(pagination);
 
 };
