808c5374bd54b9786270f34f3c11324a5e9267ab
this.Konrad <konradlebt@gmail.com>
2023-11-23 10:09:27 +0100

    get the url for names

--- components/search-bar/createSearchBar.js
+++ components/search-bar/createSearchBar.js
@@ -4,8 +4,26 @@ const searchBar = document.querySelector('[data-js="search-bar"]');
 const searchBarInput = ('.search-bar__input');
 const searchBarButton = ('.search-bar__button');
 
+let searchQuery = "";
+
 function searchBarUpdate(){
     console.log("Input Search Bar: ", searchBarInput,);
+    searchQuery = searchBarInput;
+    
 };
 
-searchBarButton-addEventListener('submit', searchBarUpdate);
+
+let fetchUrl = `https://rickandmortyapi.com/api/character/?name=${searchQuery}`;
+async function fetchCharacters(index) {
+  try {
+    const response = await fetch(fetchUrl);
+    const data = await response.json();
+    charactersFetched = data;
+  } catch (error) {
+    console.log(error);
+  }
+  return;
+}
+
+
+searchBarButton.addEventListener('submit', searchBarUpdate);
