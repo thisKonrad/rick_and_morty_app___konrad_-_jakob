46fce51f453d8265ef7febaef6cd17845e673a88
this.Konrad <konradlebt@gmail.com>
2023-11-23 09:52:16 +0100

    add variables for search bar

---
+++ components/search-bar/createSearchBar.js
@@ -0,0 +1,6 @@
+/* :::: search bar :::: */
+
+const searchBar = document.querySelector('[data-js="search-bar"]');
+
+const searchBarInput = ('.search-bar__input');
+const searchBarButton = ('.search-bar__button');
