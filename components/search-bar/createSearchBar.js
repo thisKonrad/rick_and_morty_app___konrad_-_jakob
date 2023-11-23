e8272cc6d69dc60bfe65f3b5d8bac1819c44bec0
this.Konrad <konradlebt@gmail.com>
2023-11-23 09:58:11 +0100

    add searchbarupdate function to eventlistener submit

--- components/search-bar/createSearchBar.js
+++ components/search-bar/createSearchBar.js
@@ -1,6 +1,11 @@
 /* :::: search bar :::: */
 
 const searchBar = document.querySelector('[data-js="search-bar"]');
-
 const searchBarInput = ('.search-bar__input');
 const searchBarButton = ('.search-bar__button');
+
+function searchBarUpdate(){
+    console.log("Input Search Bar: ", searchBarInput,);
+};
+
+searchBarButton-addEventListener('submit', searchBarUpdate);
